13c0ba74079125eb43a6a5394d75e3b768c1fb26
Gísli Konráð Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2026-05-04 15:53:51 +0000

    Changes SecurityTokenHandlerProvider to cache descriptors rather than instances

--- src/Protocols/WsTrust/Protocols/WsTrust/SecurityTokenHandlerProvider.cs
+++ src/Protocols/WsTrust/Protocols/WsTrust/SecurityTokenHandlerProvider.cs
@@ -7,56 +7,57 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Linq;
 using System.Text;
 
 namespace Solid.Identity.Protocols.WsTrust
 {
     public class SecurityTokenHandlerProvider : IDisposable
     {
-        private IDisposable _optionsChangeToken;
-        private IReadOnlyDictionary<Type, SecurityTokenHandler> _handlersByType;
-        private IReadOnlyDictionary<string, SecurityTokenHandler> _handlersByTokenTypeIdentifier;
-        private IServiceProvider _services;
+        private readonly IDisposable _optionsChangeToken;
+        private IReadOnlyDictionary<Type, SecurityTokenHandlerDescriptor> _descriptorsByType;
+        private IReadOnlyDictionary<string, SecurityTokenHandlerDescriptor> _descriptorsByTokenTypeIdentifier;
+        private readonly IServiceProvider _services;
 
         public SecurityTokenHandlerProvider(IServiceProvider services, IOptionsMonitor<WsTrustOptions> monitor)
         {
             _services = services;
-            _optionsChangeToken = monitor.OnChange((options, _) => UpdateSecurityTokenHandlers(options));
-            UpdateSecurityTokenHandlers(monitor.CurrentValue);
+            _optionsChangeToken = monitor.OnChange((options, _) => UpdateSecurityTokenHandlerDescriptors(options));
+            UpdateSecurityTokenHandlerDescriptors(monitor.CurrentValue);
         }
 
         public IEnumerable<SecurityTokenHandler> GetAllSecurityTokenHandlers()
-            => _handlersByType.Values;
+            => _descriptorsByType.Values.Select(d => d.Factory(_services));
 
         public SecurityTokenHandler GetSecurityTokenHandler(SecurityToken token)
             => GetSecurityTokenHandler(token?.GetType());
 
         public SecurityTokenHandler GetSecurityTokenHandler(Type tokenType)
-            => _handlersByType.TryGetValue(tokenType, out var handler) ? handler : null;
+            => _descriptorsByType.TryGetValue(tokenType, out var descriptor) ? descriptor.Factory(_services) : null;
 
         public SecurityTokenHandler GetSecurityTokenHandler(string tokenTypeIdentifier)
-            => _handlersByTokenTypeIdentifier.TryGetValue(tokenTypeIdentifier, out var handler) ? handler : null;
+            => _descriptorsByTokenTypeIdentifier.TryGetValue(tokenTypeIdentifier, out var descriptor) ? descriptor.Factory(_services) : null;
 
-        private void UpdateSecurityTokenHandlers(WsTrustOptions options)
+        private void UpdateSecurityTokenHandlerDescriptors(WsTrustOptions options)
         {
-            var handlersByTokenTypeIdentifier = new Dictionary<string, SecurityTokenHandler>();
-            var handlersByType = new Dictionary<Type, SecurityTokenHandler>();
+            var handlersByTokenTypeIdentifier = new Dictionary<string, SecurityTokenHandlerDescriptor>();
+            var handlersByType = new Dictionary<Type, SecurityTokenHandlerDescriptor>();
             foreach(var descriptor in options.SecurityTokenHandlers)
             {
                 var handler = descriptor.Factory(_services);
-                if (!(handler is AsyncSecurityTokenHandler))
+                if (handler is not AsyncSecurityTokenHandler)
                 {
                     // TODO: Remove wrapper if/when our PR for CanWriteSecurityToken default implementation gets accepted and released.
                     // https://github.com/AzureAD/azure-activedirectory-identitymodel-extensions-for-dotnet/pull/1438
                     handler = new SecurityTokenHandlerWrapper(handler);
                 }
-                handlersByType.Add(handler.TokenType, handler);
+                handlersByType.Add(handler.TokenType, descriptor);
                 foreach (var identifier in descriptor.TokenTypeIdentifiers)
-                    handlersByTokenTypeIdentifier.Add(identifier, handler);
+                    handlersByTokenTypeIdentifier.Add(identifier, descriptor);
             }
 
-            _handlersByType = new ReadOnlyDictionary<Type, SecurityTokenHandler>(handlersByType);
-            _handlersByTokenTypeIdentifier = new ReadOnlyDictionary<string, SecurityTokenHandler>(handlersByTokenTypeIdentifier);
+            _descriptorsByType = new ReadOnlyDictionary<Type, SecurityTokenHandlerDescriptor>(handlersByType);
+            _descriptorsByTokenTypeIdentifier = new ReadOnlyDictionary<string, SecurityTokenHandlerDescriptor>(handlersByTokenTypeIdentifier);
         }
 
         public void Dispose() => _optionsChangeToken.Dispose();
