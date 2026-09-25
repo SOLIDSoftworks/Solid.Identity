41b5255cdd2eec1f1bb2aca72a8ba8e7f77d239a
Gísli Konráð Björnsson <Gisli.K.Bjornsson@landsbankinn.is>
2026-09-25 15:37:21 +0000

    Remove superseded WS-Trust secure conversation stubs

--- src/IdentityModel/Protocols.WsTrust/WsSecureConversation/Identifier.cs
+++
@@ -1,6 +0,0 @@
-namespace Solid.IdentityModel.Protocols.WsSecureConversation;
-
-public class Identifier
-{
-    public string Value { get; set; }
-}
--- src/IdentityModel/Protocols.WsTrust/WsSecureConversation/SecurityContextToken.cs
+++
@@ -1,7 +0,0 @@
-namespace Solid.IdentityModel.Protocols.WsSecureConversation;
-
-public class SecurityContextToken
-{
-    public string Id { get; set; }
-    public Identifier Identifier { get; set; }
-}
